e69c232d6f430cc73c4d94bf517bdb6cdbda8dec
aydaver <Aydavercheck@gmail.com>
2025-05-04 16:25:32 +0500

    make install fix

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -4,17 +4,28 @@ import SignUp from '../features/Authentication/SignUp/ui/SignUp.jsx';
 import NotFound from '../features/NotFound/NotFound.jsx'
 import Chat from '../features/Chat/ui/Chat.jsx';
 import PrivateRoute from './PrivateRoute/PrivateRoute.jsx';
+import { Provider, ErrorBoundary } from '@rollbar/react'; 
+
+
+const rollbarConfig = {
+  accessToken: '85898ef5461f47ed8d33160bd76f9c51',
+  environment: 'testenv',
+};
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="" element={<PrivateRoute>  <Chat />  </PrivateRoute>}/>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="" element={<PrivateRoute>  <Chat />  </PrivateRoute>}/>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
