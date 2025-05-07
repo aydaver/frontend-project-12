2834497690adf0dfdf4e3cf3bf0deb5cca62aafd
aydaver <aydar.kalin@mail.ru>
2025-05-07 17:27:16 +0500

    trying to connect to webServer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     port: 5002,
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'http://localhost:5001',
       },
       '/socket.io': {
-        target: 'ws://localhost:5002',
+        target: 'ws://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
