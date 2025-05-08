0be660816e02ad0bc1c29d715958ee7b50a3066f
aydaver <aydar.kalin@mail.ru>
2025-05-08 19:18:20 +0500

    label fix

--- frontend/src/features/Authentication/Common/ui/authentication.jsx
+++ frontend/src/features/Authentication/Common/ui/authentication.jsx
@@ -36,8 +36,10 @@ const Authentication = (props) => {
     const handlePasswordCheck = () => {
         return type === 'signup' ? 
         <div className="form-group mb-4">
-            <label htmlFor="passwordCheck">{i18next.t('passwordConfirmFormTitle')}</label>
-            <Field id="passwordCheck" type="password" name="passwordCheck" className="form-control"/>
+            <label htmlFor="passwordCheck" className='w-100'>
+                {i18next.t('passwordConfirmFormTitle')}
+                <Field id="passwordCheck" type="password" name="passwordCheck" className="form-control"/>
+            </label>
             <Error name="passwordCheck">{(error) => <span className='text-danger'>{error}</span>}</Error>
         </div> 
         : 
@@ -67,14 +69,18 @@ const Authentication = (props) => {
                                     <div className="">
                                         <Form>
                                             <div className="form-group mb-4">
-                                                <label htmlFor="userName">{type === 'signup' ? i18next.t('nickNameFormTitleSignUp') : i18next.t('nickNameFormTitleLogin')}</label>
-                                                <Field type="userName" id="userName" name="userName" className="form-control"/>
+                                                <label htmlFor="userName" className='w-100'>
+                                                    {type === 'signup' ? i18next.t('nickNameFormTitleSignUp') : i18next.t('nickNameFormTitleLogin')}
+                                                    <Field type="userName" id="userName" name="userName" className="form-control"/>
+                                                </label>
                                                 <Error id="userName" name="userName">{(error) => <span className='text-danger'>{error}</span>}</Error>
                                                 <p className='text-danger my-0'>{errorState}</p>
                                             </div>
                                             <div className="form-group mb-4">
-                                                <label htmlFor="password">{i18next.t('passwordFormTitle')}</label>
-                                                <Field type="password" name="password" className="form-control"/>
+                                                <label htmlFor="password" className='w-100'>
+                                                    {i18next.t('passwordFormTitle')}
+                                                    <Field type="password" name="password" className="form-control"/>
+                                                </label>
                                                 <Error id="password" name="password">{(error) => <span className='text-danger'>{error}</span>}</Error>
                                             </div>
                                             {handlePasswordCheck()}
