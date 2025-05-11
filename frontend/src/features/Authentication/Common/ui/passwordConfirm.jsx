64dd315cfbee819fa842688ef970acd5d96169e8
aydaver <aydar.kalin@mail.ru>
2025-05-11 18:11:23 +0500

    fix

--- frontend/src/features/Authentication/Common/ui/passwordConfirm.jsx
+++ frontend/src/features/Authentication/Common/ui/passwordConfirm.jsx
@@ -1,6 +1,6 @@
-import i18next from "../../../../common/locales/i18n"
+import i18next from '../../../../common/locales/i18n'
 import {
-    Field, ErrorMessage as Error,
+  Field, ErrorMessage as Error,
 } from 'formik'
 
 const PasswordConfirm = (props) => {
@@ -8,13 +8,13 @@ const PasswordConfirm = (props) => {
 
   return type === 'signup'
     ? (
-      <div className="form-group mb-4">
-        <label htmlFor="passwordCheck" className="w-100">
-          {i18next.t('passwordConfirmFormTitle')}
-          <Field id="passwordCheck" type="password" name="passwordCheck" className="form-control" />
-        </label>
-        <Error name="passwordCheck">{error => <span className="text-danger">{error}</span>}</Error>
-      </div>
+        <div className="form-group mb-4">
+          <label htmlFor="passwordCheck" className="w-100">
+            {i18next.t('passwordConfirmFormTitle')}
+            <Field id="passwordCheck" type="password" name="passwordCheck" className="form-control" />
+          </label>
+          <Error name="passwordCheck">{error => <span className="text-danger">{error}</span>}</Error>
+        </div>
       )
     : null
 }
